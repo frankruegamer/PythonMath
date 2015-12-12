be45b4a635824a42d055904a9d974f0bee36feba
frankruegamer <frankruegamer@gmail.com>
2015-12-12 01:53:48 +0100

    Simplify Code

--- eukld.py
+++ eukld.py
@@ -4,7 +4,7 @@ import sys
 def eukld(a, m):
     y = [0, 1]
     r = [a, m]
-    q = ["  -"]
+    q = ["-"]
     while True:
         q.append(r[-2] // r[-1])
         r.append(r[-2] % r[-1])
@@ -14,7 +14,7 @@ def eukld(a, m):
     # print table
     print("\n    y     r |   q\n" + "-" * 18)
     for i in range(len(y)):
-        print("{0:5} {1:5} | {2:3}".format(y[i], r[i], q[i]))
+        print("{0:5} {1:5} | {2:>3}".format(y[i], r[i], q[i]))
     print(" " * 10 + "0 |\n")
     print("ggT({0}, {1}) = {2}".format(a, m, r[-2]))
 
